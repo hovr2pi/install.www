9e533ad8135682f13a245a23624995154e0972c9
Christopher Simmons <csim@hovr2pi.org>
2014-01-01 02:49:38 +0100

    added some updated configs for stampede and cfour

---
+++ home/install.www/configs/cfour/config.h
@@ -0,0 +1,64 @@
+
+
+#define EXENODES(runcmd) ishell('ibrun ' // runcmd)
+
+
+
+#define FC_FUNC(name,NAME) name ## _
+
+#define FC_FUNC_(name,NAME) name ## _
+
+#define FC_INT8_TYPE long int
+
+#define FC_MAIN main
+
+
+#define HAVE_GETHOSTNAME 1
+
+#define HAVE_INTTYPES_H 1
+
+
+#define HAVE_MALLOC 1
+
+#define HAVE_MEMORY_H 1
+
+#define HAVE_MPI 1
+
+#define HAVE_STDINT_H 1
+
+#define HAVE_STDLIB_H 1
+
+#define HAVE_STRINGS_H 1
+
+#define HAVE_STRING_H 1
+
+#define HAVE_SYS_STAT_H 1
+
+#define HAVE_SYS_TYPES_H 1
+
+#define HAVE_UNISTD_H 1
+
+#define PACKAGE_BUGREPORT "jonas@iki.fi"
+
+#define PACKAGE_NAME "CFOUR"
+
+#define PACKAGE_STRING "CFOUR 1.2"
+
+#define PACKAGE_TARNAME "cfour"
+
+#define PACKAGE_URL ""
+
+#define PACKAGE_VERSION "1.2"
+
+#define PAREXE(runcmd) ishell('ibrun ' // runcmd)
+
+#define SIZEOF_INT 4
+
+#define SIZEOF_LONG_INT 8
+
+#define SIZEOF_LONG_LONG_INT 8
+
+#define SIZEOF_VOID_P 8
+
+#define STDC_HEADERS 1
+
